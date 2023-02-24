c40da712215f558da8ce3278fdb29a2b4ebf033d
jashan <jashandeep.kaur@chicmic.co.in>
2023-02-24 17:34:07 +0530

    third commit

--- src/City.js
+++ src/City.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
+import "./City.css";
 function City() {
   const navigate = useNavigate();
   const { name } = useParams();
   const [data, setData] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-  const [err,setErr]=useState("")
-  const arr=[];
+  const [searchInput, setSearchInput] = useState("");
+  const [err, setErr] = useState("");
+  const [isLoading, setIsLoading] = useState(true)
+ 
   useEffect(() => {
+    setIsLoading(true)
     fetch("https://countriesnow.space/api/v0.1/countries/cities", {
       method: "POST",
       body: JSON.stringify({
@@ -20,63 +22,80 @@ function City() {
         "Content-type": "application/json; charset=UTF-8",
       },
     })
-    .then((res) => res.json())
+      .then((res) => res.json())
       .then((res) => {
-        setData(res.data)
+        setData(res.data);
+        setIsLoading(false)
       })
-      .catch(err=>setErr(err))
+      .catch((err) => {setErr(err); setIsLoading(false)});
+      
+      
   }, []);
 
 
-  const searchItems = (searchValue) => {
-    setSearchInput(searchValue)
-    if (searchValue) {
-        const filteredData = data.filter((item) => {
-            return item.toLowerCase().includes(searchInput.toLowerCase())
-        })
-        setFilteredResults(filteredData)
-    }
-    else{
-        setFilteredResults(data)
-    }
-    console.log(filteredResults)
+  useEffect(() => {
+    
+    const timer = setTimeout(() => searchItems(searchInput), 4000);
+    return () => clearTimeout(timer);
+
+}, [searchInput]);
+
+  const handleChange= (event)=>{
+    setSearchInput(event.target.value)
   }
+
+
+  const searchItems = (searchValue) => {
+    
+    setSearchInput(searchValue);
+    if (searchValue) {
+      const filteredData = data.filter((item) => {
+        return item.toLowerCase().includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(data);
+    }
+    console.log(filteredResults);
+  };
+  function displayCity(data) {
+    if(data.length){
+      return data.map((val) => <li>{val}</li>);
+    }else{
+      return <h3>"No Data Found!"</h3>;
+    }
+  }
+
+
+
   return (
     <>
-      <div>{name}</div>
-      <div>City Name</div>
-      {/* <input type="search" placeholder="Search here" onChange={handleChange} /> */}
+      <div className="content">
+        <button
+          className="btn"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        >
+         back
+        </button>
+        <br />
 
-     
+        <div className="city">Cities of {name} are:</div>
 
-
-      <input type ='search'
-                placeholder='Search...'
-                onChange={(e) => searchItems(e.target.value)}
-            />
-             <ul>{filteredResults?.map((val) => <li>{val}</li>)}</ul> 
-
-      <form>
-      
-       
-    
-      </form>
-    
-      
-      
-      <button
-        onClick={() => {
-          navigate(`/`);
-        }}
-      >
-        Back to Country
-      </button>
-      <h1>{err}</h1>
+        <input
+          className={"search"}
+          type="search"
+          placeholder="Search..."
+          onChange={handleChange}
+        />
+    {console.log(isLoading, "isLoading")}
+        {
+          isLoading ? <h3>Loading....</h3> : searchInput ? displayCity(filteredResults) : displayCity(data)
+        }
+      </div>
     </>
   );
 }
 
-
 export default City;
-
-
--- src/Join.js
+++ src/Join.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, useNavigate } from "react-router-dom";
 // import { Navigate } from "react-router-dom";
-
+import "./Join.css";
 
 const Join = () => {
   const [hasErrors, setStateErrors] = useState(false);
@@ -10,44 +10,54 @@ const Join = () => {
   const navigate = useNavigate();
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch("https://countriesnow.space/api/v0.1/countries/capital")
-      console.log(res)
-      res.json().then(res => setCountry(res.data))
-        .catch(err => setStateErrors)
+      const res = await fetch(
+        "https://countriesnow.space/api/v0.1/countries/capital"
+      );
+      console.log(res);
+      res
+        .json()
+        .then((res) => setCountry(res.data))
+        .catch((err) => setStateErrors);
     }
 
     fetchData();
-
-  }, [])
-
-
-
+  }, []);
 
   const handleSelectedCountry = (event) => {
     setSelectedCountry(event.target.value);
 
     console.log(event.target.value);
-  }
-
+  };
 
   return (
-    
-    <div>
-    <h1>Select a country</h1>
-   
-<select onChange={handleSelectedCountry} id="" name={country}>{country.map((val,index)=><option key={index} value={val.name}>{val.name}</option>)}</select>
-<button onClick={()=>{navigate(`City/${selectedCountry}`) 
+    <div class="container">
+      <h1 class="header">Select a country</h1>
 
-}}>Click</button>
-<br/>
-<br/>
+      <select onChange={handleSelectedCountry} id="" name={country}>
+        <option>Select a country</option>
+        {country.map((val, index) => (
+          <option key={index} value={val.name}>
+            {val.name}
+          </option>
+        
+        ))}
+      </select>
+      <br></br>
+      <button
+        className="btn1"
+        onClick={() => {
+          {!selectedCountry ? alert("Please select a country"):navigate(`City/${selectedCountry}`)}
+          
+        }}
+      >
+        Next
+      </button>
+      <br />
+      <br />
 
-      {/* <span>{JSON.stringify(country)}</span> */ }
       <hr />
-      {/* <span>hasErrors :{JSON.stringify(hasErrors)}</span> */}
-</div>
-  )
-
-}
+    </div>
+  );
+};
 
 export default Join;
